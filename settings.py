ae95fcb5d1f66dbf5c4d705b913b5aee76da1505
Peter Paprotny <pappet@web.de>
2023-02-15 21:43:26 +0100

    Added a configuration parser to load variables from a settings file!

---
+++ settings.py
@@ -0,0 +1,14 @@
+screen_width = 80
+screen_height = 49
+title = "Space Mining RL"
+vsync = True
+
+map_width = 80
+map_height = 43
+room_max_size = 10
+room_min_size = 6
+max_rooms = 30
+max_monsters_per_room = 2
+max_items_per_room = 2
+
+font_file = "art/dejavu10x10_gs_tc.png"
